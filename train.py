a631c31e8325a65c9531367081cb3bf50a71c53d
Neil Nie <yongyang.nie@gmail.com>
2018-09-14 21:02:10 -0400

    cleaned up logs

--- train.py
+++ train.py
@@ -18,21 +18,21 @@ import communication
 
 if __name__ == '__main__':
 
-    load_model_name = './i3d_accel_rgb_' + str(configs.LENGTH) + '_3.h5'
-    save_model_name = './i3d_accel_rgb_' + str(configs.LENGTH) + '_4.h5'
+    load_model_name = './i3d_accel_flow_' + str(configs.LENGTH) + '_1.h5'
+    save_model_name = './i3d_accel_flow_' + str(configs.LENGTH) + '_2.h5'
 
     labels = pd.read_csv('/home/neil/dataset/speedchallenge/data/data.csv').values
     val_labels = pd.read_csv('/home/neil/dataset/speedchallenge/data/validation.csv').values
 
-    i3d_flow = Inception3D(input_shape=(configs.LENGTH, configs.IMG_HEIGHT, configs.IMG_WIDTH, 3),
+    i3d_flow = Inception3D(input_shape=(configs.LENGTH, configs.IMG_HEIGHT, configs.IMG_WIDTH, 2),
                            weights_path=load_model_name)
     i3d_flow.summary()
 
-    i3d_flow.train(type='rgb_accel', labels=labels,
+    i3d_flow.train(type='flow_accel', labels=labels,
                    val_labels=val_labels,
-                   epochs=7, epoch_steps=1000,
+                   epochs=10, epoch_steps=1000,
                    validation=True, val_steps=800,
                    save_path=save_model_name,
-                   log_path='logs/rgb_64_accel')
+                   log_path='logs/flow_64_accel')
 
     communication.notify_training_completion(save_model_name)
